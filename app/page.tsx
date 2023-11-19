b0ad2f371427bac54593a225be693aa0f38cffaf
Jacobo <marcel.esplu@gmail.com>
2023-11-19 20:09:29 +0100

    NodeAppStarted

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,6 @@ export default function Home() {
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
-
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
