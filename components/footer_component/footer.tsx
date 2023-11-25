212e5b93765d79396c5f9138fca73c585fe547c7
Jacobo <marcel.esplu@gmail.com>
2023-11-25 14:31:27 +0100

    Same as the last commit, but with the addition of the tech icons.

--- components/footer_component/footer.tsx
+++ components/footer_component/footer.tsx
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-import styles from './footer.module.css'
-import { Fragment } from 'react'
+import Image from "next/image";
+import styles from "./footer.module.css";
+import { Fragment } from "react";
 
 const footer = () => {
   return (
     <Fragment>
-        {/* <div className={styles.divContGen}> */}
-          <div className={styles.footerCont}>
-            <h3>Powered by Marcel</h3>
-            <p>WebSite build in TypeScript</p>
-          </div>
-        {/* </div> */}
+      {/* <div className={styles.divContGen}> */}
+      <div className={styles.footerCont}>
+        <h3>
+          Powered by <span>Marcel</span>
+        </h3>
+        <p>WebSite build in TypeScript</p>
+      </div>
+      {/* </div> */}
     </Fragment>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
